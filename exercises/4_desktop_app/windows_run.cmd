9477bd5f166c2b7edd8db11de5c1f52d45f0f6ce
Jan Bielecki <jbiel@orsted.com>
2024-08-08 16:41:29 +0200

    windows run script

---
+++ exercises/4_desktop_app/windows_run.cmd
@@ -0,0 +1,2 @@
+# conda activate C:\Users\JBIEL\Desktop\projects\conda_envs\subinterpreters_2
+cmd /C "set QT_PLUGIN_PATH=C:\Users\JBIEL\Desktop\projects\conda_envs\subinterpreters_2\Library\plugins\platforms && python main_qt.py"
